03c919832caa56e13a3247f2e0b3e3765b125431
Chris Ridgeway <chris.ridgeway01@gmail.com>
2018-07-07 22:35:28 +0000

    Add new file

---
+++ components/New.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+                <label>New Quote:</label><br>
+        <textarea style="width: 400px; height: 100px" @keydown.enter="$bus.$emit('new-quote', $event.target.value)"></textarea>
+    </div>
+</template>
+
