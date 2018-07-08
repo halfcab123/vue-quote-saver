2d44f5ad6294c455a7be76f22153a8e68b79b640
Chris Ridgeway <chris.ridgeway01@gmail.com>
2018-07-08 14:22:11 +0000

    Progress bar to simulate freemium app limitations

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="progress">
+        <div class="progress-bar"
+             :class="[{'bg-success': totalQuotes < (0.7 * maxQuotes)},
+                       {'bg-warning': totalQuotes > (0.7 * maxQuotes)
+                                && totalQuotes < (0.9 * maxQuotes)},
+                       {'bg-danger': totalQuotes >= (0.9 * maxQuotes)}]"
+             role="progressbar"
+             :style="{'width': progress + '%'}"
+             :aria-valuenow="progress"
+             aria-valuemin="0"
+             aria-valuemax="100">
+            <span :class="{'text-dark':(progress <= 8)}">{{ totalQuotes }} of {{ maxQuotes }} Quotes Used</span></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['totalQuotes','maxQuotes'],
+        computed: {
+            progress: function(){
+                return this.totalQuotes/this.maxQuotes * 100
+            }
+        }
+    }
+</script>
